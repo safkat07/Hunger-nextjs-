e4014999b39a4d0f20ab5f2ce5f2edee6271f3cc
safkat07 <safkatsadli@gmail.com>
2024-06-10 02:47:00 +0600

    banner section complete

--- src/Components/Header/Banner/Banner.js
+++ src/Components/Header/Banner/Banner.js
@@ -1,11 +1,19 @@
 
-
+import Image from 'next/image'
+import bannerImage from '../../../../public/bannerImage.png'
 
 export default function Banner() {
     return (
-        <div style={{
-            minHeight: "calc(100vh)"
-        }} className=" text-6xl bg-red-500 pt-20">
+        <div className=" text-6xl flex justify-center bg-red-500 min-h-dvh pt-36">
+            <div className="flex relative flex-col text-[25vw] text-center  headingFont  justify-center items-center">
+                <h1 className="leading-[18vw] inline">
+                    Delicious <br />
+                    Burger
+                </h1>
+                <div className='absolute '>
+                    <Image className='xl:w-3/4 mx-auto' src={bannerImage} />
+                </div>
+            </div>
 
         </div>
     )
--- src/Components/Header/Navbar/Navbar.js
+++ src/Components/Header/Navbar/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
         setOpen(!open)
     }
     return (
-        <nav className="bg-white xl:py-5 py-3 xl:mx-5 xl:rounded-full fixed    xl:top-5 left-0 right-0">
+        <nav className="bg-white z-10 xl:py-5 py-3 xl:mx-5 xl:rounded-full fixed    xl:top-5 left-0 right-0">
             <div className="px-10 flex items-center justify-between">
                 <div className="flex  items-center gap-10">
                     <div>
@@ -25,22 +25,22 @@ export default function Navbar() {
                         <ul className="flex    items-center text-lg font-semibold uppercase gap-5">
                             <div className="relative cursor-pointer group ">
                                 <li className="  ">Home</li>
-                                <div className="w-2 h-2 mx-auto absolute  left-0 right-0  -bottom-3 transition-all duration-500 opacity-100   bg-red-600 rounded-full">
+                                <div className="w-2 h-2 mx-auto absolute  left-0 right-0  -bottom-3 transition-all duration-500 opacity-100   bg-red-500 rounded-full">
                                 </div>
                             </div>
                             <div className="relative group ">
                                 <li className="  cursor-pointer">About</li>
-                                <div className="w-2 h-2 mx-auto absolute  left-0 right-0  group-hover:-bottom-3 transition-all duration-500 group-hover:opacity-100 -bottom-full opacity-0  bg-red-600 rounded-full">
+                                <div className="w-2 h-2 mx-auto absolute  left-0 right-0  group-hover:-bottom-3 transition-all duration-500 group-hover:opacity-100 -bottom-full opacity-0  bg-red-500 rounded-full">
                                 </div>
                             </div>
                             <div className="relative group ">
                                 <li className="  cursor-pointer">Blog</li>
-                                <div className="w-2 h-2 mx-auto absolute  left-0 right-0  group-hover:-bottom-3 transition-all duration-500 group-hover:opacity-100 -bottom-full opacity-0  bg-red-600 rounded-full">
+                                <div className="w-2 h-2 mx-auto absolute  left-0 right-0  group-hover:-bottom-3 transition-all duration-500 group-hover:opacity-100 -bottom-full opacity-0  bg-red-500 rounded-full">
                                 </div>
                             </div>
                             <div className="relative group ">
                                 <li className="  cursor-pointer">Shop</li>
-                                <div className="w-2 h-2 mx-auto absolute  left-0 right-0  group-hover:-bottom-3 transition-all duration-500 group-hover:opacity-100 -bottom-full opacity-0  bg-red-600 rounded-full">
+                                <div className="w-2 h-2 mx-auto absolute  left-0 right-0  group-hover:-bottom-3 transition-all duration-500 group-hover:opacity-100 -bottom-full opacity-0  bg-red-500 rounded-full">
                                 </div>
                             </div>
 
@@ -59,7 +59,7 @@ export default function Navbar() {
                     </span>
                 </div>
 
-                <div className={`xl:hidden block w-[40%] top-0  fixed  bg-white  min-h-dvh transition-all duration-500 ${open ? "right-0" : "-right-full"}`}>
+                <div className={`xl:hidden block w-[70%] top-0  fixed  bg-white  min-h-dvh transition-all duration-500 ${open ? "right-0" : "-right-full"}`}>
 
                 </div>
             </div>
